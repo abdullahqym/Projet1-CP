dda1d36fe347286c26b052bedd313d4e054f4142
abdullahqym <muhammad.qayyum@student.uliege.be>
2025-03-04 14:04:15 +0100

    Initial Commit

---
+++ prefixe_suffixe.c
@@ -0,0 +1,9 @@
+#include <assert.h>
+#include <stdlib.h>
+
+#include "prefixe_suffixe.h"
+
+int prefixe_suffixe(int *T, const unsigned int N){
+  //Votre code ici
+  return 0;
+}//fin prefixe_suffixe()
