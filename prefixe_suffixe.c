acfea3ccdd07dac6d01785b5400e806085a4649b
abdullahqym <muhammad.qayyum@student.uliege.be>
2025-03-05 22:33:40 +0100

    idée de programme

--- prefixe_suffixe.c
+++ prefixe_suffixe.c
@@ -4,6 +4,28 @@
 #include "prefixe_suffixe.h"
 
 int prefixe_suffixe(int *T, const unsigned int N){
-  //Votre code ici
-  return 0;
-}//fin prefixe_suffixe()
+  assert(T != NULL);
+
+  unsigned int k = N-1;
+  unsigned int j = N-k;
+  unsigned int i = 0;
+  unsigned int cpt = 0;
+
+  while (k > 0){
+    while(i < k){
+      if(T[i] == T[j]){
+        i++;
+        j++;
+        cpt++;
+      }
+
+      if (T[i] != T[j]){
+        i++;
+        j++;
+        return cpt;
+      }
+    }
+  }
+
+  return cpt;
+} // fin prefixe_suffixe()
