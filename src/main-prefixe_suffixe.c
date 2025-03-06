350bf1e066a25babd69df8a1645a728d9e627b7d
abdullahqym <muhammad.qayyum@student.uliege.be>
2025-03-07 00:22:58 +0100

    code done

--- src/main-prefixe_suffixe.c
+++ src/main-prefixe_suffixe.c
@@ -3,14 +3,27 @@
 #include "prefixe_suffixe.h"
 
 int main(){
-  const unsigned int N1 = 9;
-  const unsigned int N2 = 10;
-  const unsigned int N3 = 9;
-  int T1[N1] = {1,4,2,4,5,1,4,2,4};
-  int T2[N2] = {1,2,3,2,1,1,2,3,2,1};
-  int T3[N3] = {3,2,3,2,1,2,3,2,1};
+  // const unsigned int N1 = 9;
+  // const unsigned int N2 = 10;
+  // const unsigned int N3 = 9;
+  // int T1[N1] = {1,4,2,4,5,1,4,2,4};
+  // int T2[N2] = {1,2,3,2,1,1,2,3,2,1};
+  // int T3[N3] = {3,2,3,2,1,2,3,2,1};
 
-  printf("Longueur plus long préfixe/suffixe de T1: %u\n", prefixe_suffixe(T1, N1));
-  printf("Longueur plus long préfixe/suffixe de T2: %u\n", prefixe_suffixe(T2, N1));
-  printf("Longueur plus long préfixe/suffixe de T3: %u\n", prefixe_suffixe(T3, N1));
+  // printf("Longueur plus long préfixe/suffixe de T1: %u\n", prefixe_suffixe(T1, N1));
+  // printf("Longueur plus long préfixe/suffixe de T2: %u\n", prefixe_suffixe(T2, N2));
+  // printf("Longueur plus long préfixe/suffixe de T3: %u\n", prefixe_suffixe(T3, N3));
+  
+  int T4[10] = {5, 6, 7, 6, 5, 5, 6, 7, 6, 5};
+  int T5[7] = {9, 8, 7, 6, 7, 8, 9};
+  int T6[7] = {4, 4, 4, 4, 4, 4, 4};
+  int T7[6] = {1, 2, 3, 4, 5, 6};
+  int T8[14] = {7, 6, 5, 4, 3, 2, 1, 7, 6, 5, 4, 3, 2, 1};
+
+  printf("Longueur plus long préfixe/suffixe de T4: %u\n", prefixe_suffixe(T4, 10));
+  printf("Longueur plus long préfixe/suffixe de T5: %u\n", prefixe_suffixe(T5, 7));
+  printf("Longueur plus long préfixe/suffixe de T6: %u\n", prefixe_suffixe(T6, 7));
+  printf("Longueur plus long préfixe/suffixe de T7: %u\n", prefixe_suffixe(T7, 6));
+  printf("Longueur plus long préfixe/suffixe de T8: %u\n", prefixe_suffixe(T8, 14));
+
 }
--- src/prefixe_suffixe.c
+++ src/prefixe_suffixe.c
@@ -1,31 +1,42 @@
 #include <assert.h>
 #include <stdlib.h>
+#include <stdio.h>
 
 #include "prefixe_suffixe.h"
 
-int prefixe_suffixe(int *T, const unsigned int N){
-  assert(T != NULL);
+int prefixe_suffixe(int *T, const unsigned int N)
+{
+    assert(T != NULL);
 
-  unsigned int k = N-1;
-  unsigned int j = N-k;
-  unsigned int i = 0;
-  unsigned int cpt = 0;
-
-  while (k > 0){
-    while(i < k){
-      if(T[i] == T[j]){
-        i++;
-        j++;
-        cpt++;
-      }
-
-      if (T[i] != T[j]){
-        i++;
-        j++;
-        return cpt;
-      }
+    unsigned int suff = N / 2;
+    if (N % 2 != 0)
+    {
+        suff++;
     }
-  }
 
-  return cpt;
+    unsigned int preff = 0;
+    unsigned int cpt = 0;
+
+    while (preff < suff && suff < N)
+    {
+        if (T[suff] == T[preff])
+        {
+            suff++;
+            preff++;
+            cpt++;
+            if (cpt == N / 2 && N % 2 != 0)
+            {
+                return 0;
+            }
+        }
+
+        else if (T[0] == T[N - 1])
+            return 1;
+            
+        else
+            return cpt;
+    }
+
+    return cpt;
+
 } // fin prefixe_suffixe()
